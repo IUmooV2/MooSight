352bf2a65feacada318e9b65925f347c8f7ae92f
IUmooV2 <action528@gmail.com>
2026-08-11 19:29:27 -0400

    Expand account finder unit tests

--- test/unit/modules/test_sfp_accounts.py
+++ test/unit/modules/test_sfp_accounts.py
@@ -24,3 +24,27 @@ class TestModuleAccounts(unittest.TestCase):
     def test_producedEvents_should_return_list(self):
         module = sfp_accounts()
         self.assertIsInstance(module.producedEvents(), list)
+
+    def test_normalize_username_preserves_periods_and_strips_quotes(self):
+        self.assertEqual(sfp_accounts._normalize_username('  "sonrie.99"  '), 'sonrie.99')
+
+    def test_normalize_username_rejects_path_like_and_control_input(self):
+        invalid = [None, '', '   ', 'name/other', 'name\\other', 'name\nother', 'name\tother']
+        for value in invalid:
+            with self.subTest(value=value):
+                self.assertIsNone(sfp_accounts._normalize_username(value))
+
+    def test_host_only_accepts_parseable_hostname(self):
+        self.assertEqual(sfp_accounts._host('https://instagram.com/example'), 'instagram.com')
+        self.assertEqual(sfp_accounts._host('not-a-url'), '')
+
+    def test_generate_permutations_is_conservative_and_deterministic(self):
+        module = sfp_accounts()
+        self.assertEqual(
+            module.generatePermutations('sample'),
+            ['-sample', '_sample', 'sample-', 'sample_']
+        )
+
+    def test_checkSites_rejects_invalid_username_without_network_requests(self):
+        module = sfp_accounts()
+        self.assertEqual(module.checkSites('bad/name', []), [])
